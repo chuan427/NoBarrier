99a4118171b8bdadd46936f64f9837ce3580db39
PIN CHIEH HSU <landyhsu1122@gmail.com>
2024-03-15 14:40:52 +0800

    1.新增orderMVC 2.修改訂單狀態網頁

---
+++ src/main/java/com/order/model/OrderService.java
@@ -0,0 +1,62 @@
+package com.order.model;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.limitsale.model.LimitSaleVO;
+import com.quo.model.QuoVO;
+import com.reqorder.model.ReqOrderVO;
+
+
+
+@Service("orderService")
+public class OrderService {
+
+
+		@Autowired
+		OrderRepository repository;
+
+		public void addOrder(OrderVO OrderVO) {
+			repository.save(OrderVO);
+		}
+
+		public void updateOrder(OrderVO OrderVO) {
+			repository.save(OrderVO);
+		}
+		//改變訂單狀態
+		public void changeStatByOrderId(OrderVO OrderVO) {
+			repository.save(OrderVO);
+		}
+
+		public void deleteOrder(Integer ordNum) {
+			if (repository.existsById(ordNum))
+				repository.deleteByOrderId(ordNum);
+			    repository.deleteById(ordNum);
+		}
+
+		public OrderVO getOneOrder(Integer ordNum) {
+			Optional<OrderVO> optional = repository.findById(ordNum);
+//			return optional.get();
+			return optional.orElse(null);  // public T orElse(T other) : 如果值存在就回傳其值，否則回傳other的值
+		}
+
+		public List<OrderVO> getAll() {
+			return repository.findAll();
+		}
+		//訂單對需求一對一
+		public ReqOrderVO getReqOrderByordReqnum(Integer reqNum){
+			return getOneOrder(reqNum).getReqOrderVO();
+		}
+//		//訂單對報價一對一
+		public QuoVO getQuoByordQuonum(Integer quoNum){
+			return getOneOrder(quoNum).getQuoVO();
+		}
+		//訂單對特賣一對一
+		public LimitSaleVO getLimitSaleByordLimnum(Integer limNum){
+			return getOneOrder(limNum).getLimitsaleVO();
+		}
+}
